cc0c3c3e243e3906308b0f5f2602aba4571056d8
Saulm <Saulm@SAUL>
2023-08-24 23:31:29 -0600

    Agregar archivos de proyecto.

---
+++ ResolverProblema1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolverProblema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero=0, numero2=0;
+            Console.WriteLine("Introduce un número para obtener su raiz cuadrada");
+            numero = int.Parse(Console.ReadLine());
+            numero2 = numero;
+            double raizCuadrada = Math.Sqrt(numero);
+
+            Console.WriteLine($"La raíz cuadrada de {numero} es: {raizCuadrada}");
+
+            
+            double raizCuadrada2 = Math.Pow(numero2, 0.5);
+
+            Console.WriteLine($"La raíz cuadrada de {numero2} es: {raizCuadrada2}");
+            Console.ReadKey();
+        }
+    }
+}
